e5d4f8bb9bf4edcda16bea9afcbeb3002a1d890a
WinterCore <hogobbl@gmail.com>
2020-07-21 22:49:42 +0200

    Add edit user and improve code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,43 @@
 class UsersController < ApplicationController
-	def new
-		
-	end
+  before_action :set_user, only: %i[edit update]
 
-	def create
-		@user = User.new(user_params)
+  def index
+    @users = User.all
+  end
 
-		if @user.save
-			redirect_to new_user_path
-		else
-			render :new
-		end
-	end
+  def new
+    @user = User.new
+  end
 
-	private
+  def create
+    @user = User.new(user_params)
 
-	def user_params
-		params.permit :username, :email, :password
-	end
+    if @user.save
+      flash[:notice] = 'User created successfully'
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
+  def edit; end
 
+  def update
+    if @user.update user_params
+      flash[:notice] = 'User updated successfully'
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :username, :email, :password
+  end
+
+  def set_user
+    @user = User.find params[:id]
+  end
 end
